62042d4a329d215663875ce472e2bdceaf48d6ad
Rahul Nirwan <rahulnirwan44@gmail.com>
2021-10-08 16:49:22 +0530

    add cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from "react"
 
+import Cartitem from './Cartitem'
+
 function App() {
     return ( <
         div className = "App" >
+
+
         <
-        h1 > Cart < /h1> < /
+        Cartitem / >
+        <
+        /
         div >
     );
 }
