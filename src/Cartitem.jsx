32f20561a270ea9f198ca1f348a26b4e87212195
Rahul Nirwan <rahulnirwan44@gmail.com>
2021-10-09 20:03:34 +0530

    create increase quantity function

--- src/Cartitem.jsx
+++ src/Cartitem.jsx
@@ -3,11 +3,30 @@ import React from 'react';
 
 class CartItem extends React.Component {
 
+    constructor() {
+
+        super();
+
+        this.state = {
+
+            price: 999,
+            title: 'Phone',
+            qty: 1,
+            img: ''
+        }
+    }
+
+    increaseQuantity = () => {
+
+        console.log("this state", this.state)
+    }
 
     render() {
 
+        const { price, title, qty } = this.state;
 
         return (
+
 
             <
             div className = "cart-item" >
@@ -25,18 +44,33 @@ class CartItem extends React.Component {
             <
             div style = {
                 { fontSize: 25 }
-            } > Phone < /div> <
+            } > { title } < /div> <
             div style = {
                 { color: "grey" }
-            } > Rs 999 < /div> <
+            } > Rs { price } < /div> <
             div style = {
                 { color: "grey" }
-            } > Qty: 1 < /div>
+            } > Qty: { qty } < /div>
 
 
             <
             div className = "cart-item-actions" >
 
+
+            <
+            img alt = "increase"
+            className = "action-icons"
+            src = "https://cdn-icons-png.flaticon.com/512/992/992651.png"
+            onClick = { this.increaseQuantity }
+
+            / > <
+            img alt = "decrease"
+            className = "action-icons"
+            src = "https://cdn-icons-png.flaticon.com/512/992/992683.png" / >
+            <
+            img alt = "delete"
+            className = "action-icons"
+            src = "https://cdn-icons-png.flaticon.com/512/1214/1214428.png" / >
 
             <
             /div>
